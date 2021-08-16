026744e0611b460e51d925133c80d0a8a6301b80
Zeyu Zhou <zeyu.zhou.19@ucl.ac.uk>
2021-08-16 13:07:23 +0800

    D16

---
+++ src/Y2021/M08/D16/MySolution.java
@@ -0,0 +1,10 @@
+package Y2021.M08.D16;
+
+//https://leetcode-cn.com/problems/beautiful-arrangement/
+
+public class MySolution {
+    public int countArrangement(int n)
+    {
+        return 0;
+    }
+}
